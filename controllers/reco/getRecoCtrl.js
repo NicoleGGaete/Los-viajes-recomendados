9bc8a289fdd04d242e0f749c27f54cbbe68d82d7
Inaki Garde <igarde91@gmail.com>
2022-05-03 17:53:26 +0200

    Get de Recomendacion por Id

---
+++ controllers/reco/getRecoCtrl.js
@@ -0,0 +1,20 @@
+const { getRecoId } = require('../../db/reco/getRecoId');
+
+// app.get('/reco/:id', getRecoCtrl); //ver el detalle de una reco por ID
+const getRecoCtrl = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const reco = await getRecoId(id);
+
+    res.send({
+      status: 'ok',
+      data: reco,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getRecoCtrl,
+};
---
+++ db/reco/getRecoId.js
@@ -0,0 +1,27 @@
+const { genError } = require('../../helpers/helpers');
+const { getConnection } = require('../db');
+
+const getRecoId = async (recoId) => {
+  let connection;
+  try {
+    connection = await getConnection();
+
+    const [result] = await connection.query(
+      `
+        SELECT * FROM reco WHERE recoId = ?
+        `,
+      [recoId]
+    );
+    if (result.length === 0) {
+      throw await genError(
+        `La recomendacion con el ID: ${recoId} no existe`,
+        404
+      );
+    }
+    return result[0];
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+module.exports = { getRecoId };
