c95a92ac8130f85fc7eaaff559026e05c3c83c00
Inaki Garde <igarde91@gmail.com>
2022-06-04 01:33:51 +0100

    agregado endpoint getMe y cambios varios

--- validators/users/nwUsrSchm.js
+++ validators/users/nwUsrSchm.js
@@ -7,21 +7,22 @@ const nwUsrSchm = Joi.object().keys({
     'string.email': 'Debe ingresar un email valido',
     'any.required': 'El email es un campo requerido',
   }),
-  password: Joi.string().min(7).required().messages({
+  password1: Joi.string().min(7).required().messages({
     'string.empty': 'Debe ingresar un password',
     'string.min': 'El password debe contener al menos 7 caraceres',
     'any.required': 'El password es un campo requerido',
   }),
-  confirmPas: Joi.string().valid(Joi.ref('password')).required().messages({
+  password2: Joi.string().valid(Joi.ref('password1')).required().messages({
     'string.empty': 'Debe ingresar un password de confirmacion',
     'any.only':
       'El password de confirmacion tiene que coincidir con el password',
-    'any.required': 'El confirmPas es un campo requerido',
+    'any.required': 'La confirmacion de password es requerida',
   }),
 
   userName: Joi.string().token().max(100).required().messages({
     'string.empty': 'Debe ingresar un nombre de usuario',
-    'string.token': 'Debe contener solo a-z, A-Z, 0-9 y _ ',
+    'string.token':
+      'El nombre de usuario debe contener solo a-z, A-Z, 0-9 y _ ',
     'string.max': 'El nombre de usuario no debe tener más de 100 caracteres',
     'any.required': 'El nombre de usuario es un campo requerido',
   }),
@@ -38,8 +39,8 @@ const nwUsrSchm = Joi.object().keys({
     'any.required': 'El apellido de usuario es un campo requerido',
   }),
 
-  description: Joi.string().max(280).messages({
-    'string.empty': 'Debe ingresar una descripcion',
+  description: Joi.string().optional().max(280).messages({
+    // 'string.empty': 'Debe ingresar una descripcion',
     'string.max': 'La descripcion debe contener menos de 280 caraceres',
   }),
 });
